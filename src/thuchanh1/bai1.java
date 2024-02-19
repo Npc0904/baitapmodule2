841f0cc6e44096238a54e568019c92182b7010e7
TuNPAC <141643257+Npc0904@users.noreply.github.com>
2024-02-20 08:11:17 +0900

    update
    update

---
+++ src/thuchanh1/bai1.java
@@ -0,0 +1,25 @@
+package thuchanh1;
+
+import java.util.Scanner;
+
+public class bai1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year;
+        System.out.println("Enter a year: ");
+        year = scanner.nextInt();
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println(" %d is a leap year", year);
+                } else {
+                    System.out.println("%d is NOT a leap year", year);
+                }
+            }else{
+                    System.out.println("%d is a leap year", year);
+                }
+            }else
+                System.out.printf("%d is NOT a leap year", year);
+            }
+        }
+    }
---
+++ src/thuchanh1/bai2.java
@@ -0,0 +1,7 @@
+package thuchanh1;
+
+public class bai2 {
+    public static void main(String[] args) {
+        
+    }
+}
