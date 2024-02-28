16e2305481efe3e2ccf70645a5f2699124dc6c06
TuNPAC <141643257+Npc0904@users.noreply.github.com>
2024-02-29 08:19:46 +0900

    Update Main.java

--- src/bai6/Main.java
+++ src/bai6/Main.java
@@ -2,7 +2,7 @@ package bai6;
 
 import java.util.Scanner;
 
-public class Test {
+public class Main {
         public static void main(String[] args) {
             Product product = new Product();
             Scanner sc = new Scanner(System.in);
