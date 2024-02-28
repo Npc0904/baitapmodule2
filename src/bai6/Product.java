911b6080338986570e640f74bc7febc5b39b3211
TuNPAC <141643257+Npc0904@users.noreply.github.com>
2024-02-29 08:16:29 +0900

    update
    update

---
+++ src/bai6/Product.java
@@ -0,0 +1,77 @@
+package bai6;
+
+import java.util.Scanner;
+
+public class Product {
+        private String Id;
+        private String Name;
+        private Float Price;
+        private Float Import;
+
+        public Product() {
+        }
+
+        public Product(String Id, String Name, Float Price, Float Import) {
+            this.Id = Product.this.Id;
+            this.Name = Product.this.Name;
+            this.Price = Price;
+            this.Import = Product.this.Import;
+        }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Float getPrice() {
+        return Price;
+    }
+
+    public void setPrice(Float price) {
+        Price = price;
+    }
+
+    public Float getImport() {
+        return Import;
+    }
+
+    public void setImport(Float Import) {
+            this.Import = Product.this.Import;
+        }
+        //    phương thức nhập thông tin sản phẩm
+        public void enterInfo(){
+            Scanner sc = new Scanner(System.in);
+            do {
+                System.out.println("Nhap thong tin san pham ");
+                System.out.println("Ten san pham");
+                Name = sc.nextLine();
+                System.out.println("Ma san pham");
+                Id = sc.nextLine();
+                System.out.println("Gia nhap");
+                Import = new Scanner(System.in).nextFloat();
+                System.out.println("Gia ban");
+                Price = new Scanner(System.in).nextFloat();
+                System.out.println("Ban co muon nhap tiep tuc? (c/k)");
+                String line = sc.nextLine();
+                if (line.equalsIgnoreCase("k"))
+                    break;
+            } while (true);
+        }
+        public void showInfo(){
+            System.out.println("San pham: "+Name+" Ma : "+Id+" Gia ban: "+Price +" $"+" Gia nhap: "+Import+" $");
+        }
+        public Float interestRate(){
+            return Price -Import;
+        }
+    }
---
+++ src/bai6/Test.java
@@ -0,0 +1,36 @@
+package bai6;
+
+import java.util.Scanner;
+
+public class Test {
+        public static void main(String[] args) {
+            Product product = new Product();
+            Scanner sc = new Scanner(System.in);
+            do {
+                System.out.println(" =============MEnu===========\n" +
+                        "    \n" +
+                        "1.Nhập vào thông tin sản phâm\n" +
+                        "2.Hiển thị thông tin \n" +
+                        "3.Hiển thị lãi xuất \n" +
+                        "4.Thoát");
+                System.out.println("\n Nhap lua chon cya ban");
+                int number = sc.nextInt();
+                switch (number){
+                    case 1:
+                        product.enterInfo();
+                        break;
+                    case 2:
+                        product.showInfo();
+                        break;
+                    case 3:
+                        System.out.println("lai suat la: "+product.interestRate()+"$");
+                        break;
+                    case 4:
+                        System.exit(0);
+                    default:
+                        System.out.println("Moi ban nhap lai lua chon");
+                        break;
+                }
+            } while (true);
+        }
+    }
